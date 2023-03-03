ca0f510e69e8d9837c251ce225403a2c98ab0818
Gautam <ganpatigautam08@gmail.com>
2023-03-03 21:33:49 +0530

    Final

--- lib/car_list.dart
+++ lib/car_list.dart
@@ -1,4 +1,3 @@
-import 'package:cardetail_project/cardatamodel.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'widgets/list_widget.dart';
@@ -11,12 +10,10 @@ class CarList extends StatefulWidget {
 }
 
 class _CarListState extends State<CarList> {
+  final columns = ['Car Name', 'Color', 'Type', 'Model', 'Cost'];
 
   @override
   Widget build(BuildContext context) {
-    final columns = ['Car Name', 'Color', 'Type', 'Model', 'Cost'];
-    final rows = [];
-
     return Scaffold(
       appBar: AppBar(
         elevation: 5,
@@ -24,205 +21,168 @@ class _CarListState extends State<CarList> {
         title: Text('Car Detail List'),
         centerTitle: false,
       ),
-      body: ScrollableWidget(
-        child: DataTable(columns: getColumns(columns), rows: <DataRow>[
-          DataRow(cells: <DataCell>[
-            DataCell(Text('Aston')),
-            DataCell(Text('Black')),
-            DataCell(Text('AC')),
-            DataCell(Text('S15')),
-            DataCell(Text('2000000')),
-          ]),
-          DataRow(cells: <DataCell>[
-            DataCell(Text('Aston')),
-            DataCell(Text('Black')),
-            DataCell(Text('AC')),
-            DataCell(Text('S15')),
-            DataCell(Text('2000000')),
-          ]),
-          DataRow(cells: <DataCell>[
-            DataCell(Text('Aston')),
-            DataCell(Text('Black')),
-            DataCell(Text('AC')),
-            DataCell(Text('S15')),
-            DataCell(Text('2000000')),
-          ]),
-          DataRow(cells: <DataCell>[
-            DataCell(Text('Aston')),
-            DataCell(Text('Black')),
-            DataCell(Text('AC')),
-            DataCell(Text('S15')),
-            DataCell(Text('2000000')),
-          ]),
-          DataRow(cells: <DataCell>[
-            DataCell(Text('Aston')),
-            DataCell(Text('Black')),
-            DataCell(Text('AC')),
-            DataCell(Text('S15')),
-            DataCell(Text('2000000')),
-          ]),
-          DataRow(cells: <DataCell>[
-            DataCell(Text('Aston')),
-            DataCell(Text('Black')),
-            DataCell(Text('AC')),
-            DataCell(Text('S15')),
-            DataCell(Text('2000000')),
-          ]),
-          DataRow(cells: <DataCell>[
-            DataCell(Text('Aston')),
-            DataCell(Text('Black')),
-            DataCell(Text('AC')),
-            DataCell(Text('S15')),
-            DataCell(Text('2000000')),
-          ]),
-          DataRow(cells: <DataCell>[
-            DataCell(Text('Aston')),
-            DataCell(Text('Black')),
-            DataCell(Text('AC')),
-            DataCell(Text('S15')),
-            DataCell(Text('2000000')),
-          ]),
-          DataRow(cells: <DataCell>[
-            DataCell(Text('Aston')),
-            DataCell(Text('Black')),
-            DataCell(Text('AC')),
-            DataCell(Text('S15')),
-            DataCell(Text('2000000')),
-          ]),
-          DataRow(cells: <DataCell>[
-            DataCell(Text('Aston')),
-            DataCell(Text('Black')),
-            DataCell(Text('AC')),
-            DataCell(Text('S15')),
-            DataCell(Text('2000000')),
-          ]),
-          DataRow(cells: <DataCell>[
-            DataCell(Text('Aston')),
-            DataCell(Text('Black')),
-            DataCell(Text('AC')),
-            DataCell(Text('S15')),
-            DataCell(Text('2000000')),
-          ]),
-          DataRow(cells: <DataCell>[
-            DataCell(Text('Aston')),
-            DataCell(Text('Black')),
-            DataCell(Text('AC')),
-            DataCell(Text('S15')),
-            DataCell(Text('2000000')),
-          ]),
-          DataRow(cells: <DataCell>[
-            DataCell(Text('Aston')),
-            DataCell(Text('Black')),
-            DataCell(Text('AC')),
-            DataCell(Text('S15')),
-            DataCell(Text('2000000')),
-          ]),
-          DataRow(cells: <DataCell>[
-            DataCell(Text('Aston')),
-            DataCell(Text('Black')),
-            DataCell(Text('AC')),
-            DataCell(Text('S15')),
-            DataCell(Text('2000000')),
-          ]),
-          DataRow(cells: <DataCell>[
-            DataCell(Text('Aston')),
-            DataCell(Text('Black')),
-            DataCell(Text('AC')),
-            DataCell(Text('S15')),
-            DataCell(Text('2000000')),
-          ]),
-          DataRow(cells: <DataCell>[
-            DataCell(Text('Aston')),
-            DataCell(Text('Black')),
-            DataCell(Text('AC')),
-            DataCell(Text('S15')),
-            DataCell(Text('2000000')),
-          ]),
-          DataRow(cells: <DataCell>[
-            DataCell(Text('Aston')),
-            DataCell(Text('Black')),
-            DataCell(Text('AC')),
-            DataCell(Text('S15')),
-            DataCell(Text('2000000')),
-          ]),
-          DataRow(cells: <DataCell>[
-            DataCell(Text('Aston')),
-            DataCell(Text('Black')),
-            DataCell(Text('AC')),
-            DataCell(Text('S15')),
-            DataCell(Text('2000000')),
-          ]),
-        ],
-        )),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection('car').snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return Text('Something went wrong');
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return CircularProgressIndicator();
+          }
+
+          final rows = snapshot.data!.docs.map((QueryDocumentSnapshot doc) {
+            Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+            return DataRow(
+              cells: <DataCell>[
+                DataCell(Text(data['car name'])),
+                DataCell(Text(data['color'])),
+                DataCell(Text(data['car type'])),
+                DataCell(Text(data['model'])),
+                DataCell(Text(data['cost'].toString())),
+              ],
+              onSelectChanged: (value) {
+                showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: Text('Edit/Delete Row'),
+                        content:
+                            Text('What would you like to do with this row?'),
+                        actions: <Widget>[
+                          TextButton(
+                            onPressed: () {
+                              FirebaseFirestore.instance
+                                  .collection('car')
+                                  .doc(doc.id)
+                                  .delete();
+                              Navigator.of(context).pop();
+                            },
+                            child: Text('Delete'),
+                          ),
+                          TextButton(
+                              child: Text('Edit'),
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                                showDialog(
+                                    context: context,
+                                    builder: (BuildContext context) {
+                                      final TextEditingController
+                                          carNameController =
+                                          TextEditingController(
+                                              text: data['car name']);
+                                      final TextEditingController
+                                          colorController =
+                                          TextEditingController(
+                                              text: data['color']);
+                                      final TextEditingController
+                                          carTypeController =
+                                          TextEditingController(
+                                              text: data['car type']);
+                                      final TextEditingController
+                                          modelController =
+                                          TextEditingController(
+                                              text: data['model']);
+                                      final TextEditingController
+                                          costController =
+                                          TextEditingController(
+                                              text: data['cost'].toString());
+                                      return AlertDialog(
+                                        title: Text('Edid Row'),
+                                        content: SingleChildScrollView(
+                                          child: Column(
+                                            mainAxisSize: MainAxisSize.min,
+                                            children: <Widget>[
+                                              TextField(
+                                                controller: carNameController,
+                                                decoration: InputDecoration(
+                                                  labelText: 'Car Name',
+                                                ),
+                                              ),
+                                              TextField(
+                                                controller: colorController,
+                                                decoration: InputDecoration(
+                                                  labelText: 'Color',
+                                                ),
+                                              ),
+                                              TextField(
+                                                controller: carTypeController,
+                                                decoration: InputDecoration(
+                                                  labelText: 'Type',
+                                                ),
+                                              ),
+                                              TextField(
+                                                controller: modelController,
+                                                decoration: InputDecoration(
+                                                  labelText: 'Model',
+                                                ),
+                                              ),
+                                              TextField(
+                                                controller: costController,
+                                                decoration: InputDecoration(
+                                                  labelText: 'Cost',
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                        actions: <Widget>[
+                                          TextButton(
+                                            child: Text('Cancel'),
+                                            onPressed: () {
+                                              Navigator.of(context).pop();
+                                            },
+                                          ),
+                                          TextButton(
+                                            child: Text('Update'),
+                                            onPressed: () {
+                                              FirebaseFirestore.instance
+                                                  .collection('car')
+                                                  .doc(doc.id)
+                                                  .update({
+                                                    'car name':
+                                                        carNameController.text,
+                                                    'color':
+                                                        colorController.text,
+                                                    'car type':
+                                                        carTypeController.text,
+                                                    'model':
+                                                        modelController.text,
+                                                    'cost': int.parse(
+                                                        costController.text),
+                                                  })
+                                                  .then((value) =>
+                                                      Navigator.of(context)
+                                                          .pop())
+                                                  .catchError((error) => print(
+                                                      'Failed to update row: $error'));
+                                            },
+                                          )
+                                        ],
+                                      );
+                                    });
+                              }),
+                        ],
+                      );
+                    });
+              },
+            );
+          }).toList();
+
+          return ScrollableWidget(
+            child: DataTable(columns: getColumns(columns), rows: rows),
+          );
+        },
+      ),
     );
   }
 
   List<DataColumn> getColumns(List<String> columns) {
-  return columns.map((String Column) {
-    return DataColumn(
-      label: Text(Column),
+    return columns.map((String column) {
+      return DataColumn(
+        label: Text(column),
       );
-  }).toList();
-  }
-
-  // List<DataRow> getRows(List<DataRow> rows) => cars.map((Car car) {
-  //   final cells = [car.Name, car.Color, car.Model, car.Type, car.Cost];
-
-  //   return DataRow(
-  //     cells: Utils.modelBuilder(cells, (index, cell) {
-  //       return DataCell(Text('$rows'),
-  //       showEditIcon: true,
-  //       );
-  //     }),
-  //   );
-  // }
-  // );
-
-}
-
-class GetRow extends StatefulWidget {
-  const GetRow({super.key});
-
-  @override
-  State<GetRow> createState() => _GetRowState();
-}
-
-class _GetRowState extends State<GetRow> {
-  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-
-  
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder<QuerySnapshot>(
-      stream: FirebaseFirestore.instance.collection('car').snapshots(),
-      builder:(BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-        if(!snapshot.hasData) {
-          return Center(
-            child: CircularProgressIndicator(),
-          );
-        }
-        List<DataRow> rows =
-              snapshot.data!.docs.map((QueryDocumentSnapshot doc) {
-            Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
-            return DataRow(cells: [
-              DataCell(Text(data['car name'])),
-              DataCell(Text(data['color'])),
-              DataCell(Text(data['car type'])),
-              DataCell(Text(data['model'])),
-              DataCell(Text('\$${data['cost']}')),
-            ]);
-          }).toList();
-          print(rows);
-          return DataTable(
-            columns: [
-              DataColumn(label: Text('Name')),
-              DataColumn(label: Text('Color')),
-              DataColumn(label: Text('Type')),
-              DataColumn(label: Text('Model')),
-              DataColumn(label: Text('Cost')),
-            ],
-            rows: rows,
-          );
-      });
+    }).toList();
   }
 }
--- lib/widgets/list_widget.dart
+++ lib/widgets/list_widget.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 
 class Utils {
 static List<T> modelBuilder<M, T>(
